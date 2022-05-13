647709e26b61c4b482f15f27a9d029fbbf29ecca
inbharajmani <inbharaj.m@gmail.com>
2022-05-13 22:42:11 +0530

    elderring game if condition added

---
+++ eldenring_game.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "Entering the game ..."
+sleep 3
+
+no=$(( $RANDOM % 2 ))
+
+echo "Enter your guess:"
+
+read guess_no
+if [ $no == $guess_no ]
+then
+	echo "You guessed correctly"
+else
+	echo "You did wrong"
+fi 
+
+echo "Random number is $no"
