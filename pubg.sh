425c1baf36ed8e263dc6652b9dabbdfaa7be6d4c
root <root@ip-172-31-12-242.ap-south-1.compute.internal>
2022-05-16 08:54:48 +0000

    pupg modified

--- pubg.sh
+++ pubg.sh
@@ -1,18 +1,41 @@
 #!/bin/bash
 
-echo "Entering the game ..."
-sleep 3
+echo "Entering game... Hang on..."
+sleep 2
+control=test
+until [ $control == "EXIT" ]
+do
+gun=$(( $RANDOM % 3 ))
+gun2=$(( $RANDOM % 3 ))
+case $gun in
+	0) 
+		Name="AKM"
+		Power=9
+		Range=6
+		;;
+	1)
+		Name="M416"
+		Power=8
+		Range=8
+		;;
+	2)
+		Name="Mk14"
+		Power=7
+		Range=8
+		;;
+esac
 
-no=$(( $RANDOM % 2 ))
-
-echo "Enter your guess (0/1)"
-
-read guess_no
-if [ $no == $guess_no ]
+if [ $gun -gt $gun2 ]
 then
-	echo "You guessed correctly"
+	echo "You won the game!!!"
+	echo "Your gun is $Name which has power $Power out of 10 and Range $Range out of 10"
+elif [ $gun == $gun2 ]
+then
+	continue
 else
-	echo "You did wrong"
+	echo "You lost"
 fi 
 
-echo "Random number is $no"
+echo "Enter EXIT to quit the game"
+read control
+done
