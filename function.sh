355c921be38a7fa8ff4ae31410cbae6e97b43f18
Inbharaj <inbharaj.m@gmail.com>
2022-09-21 15:38:12 +0530

    Create function.sh

---
+++ function.sh
@@ -0,0 +1,11 @@
+function add() {
+  echo $(( $1 + $2 ))
+}
+
+result=$(add 3 5)
+echo "The result is $result"function add() {
+  echo $(( $1 + $2 ))
+}
+
+result=$(add 3 5)
+echo "The result is $result"
